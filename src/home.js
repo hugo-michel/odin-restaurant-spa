701407440aff116dff2f8640f123ee31ba0cd4e3
hugo-michel <orayito62@gmail.com>
2024-03-22 16:27:47 +0100

    debut de module home

---
+++ src/home.js
@@ -0,0 +1,15 @@
+export default function showHome() {
+    const contentDiv = document.querySelector("#content");
+    
+    let homeDiv = document.createElement('div');
+    let h1 = document.createElement("h1");
+
+    h1.textContent = "Welcome to Big Burger Restaurant !"
+
+    homeDiv.appendChild(h1);
+
+    contentDiv.appendChild(homeDiv);
+
+    return contentDiv;
+
+}
