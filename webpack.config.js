6fdf6cedd59e6ce8a64355100bec6871597e204b
hugo-michel <orayito62@gmail.com>
2024-03-22 15:28:42 +0100

    config webpack css

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	mode: 'development',
+	mode: "development",
 	entry: "./src/index.js",
-	devtool: 'inline-source-map',
+	devtool: "inline-source-map",
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist"),
@@ -12,10 +12,18 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: './src/index.html',
-			filename: 'index.html',
-			inject: 'head',
-			scriptLoading: 'defer',
-		})
-	]
+			template: "./src/index.html",
+			filename: "index.html",
+			inject: "head",
+			scriptLoading: "defer",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
 };
